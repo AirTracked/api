1dcdae005ef70bb453ca9b23154f89fbf36ddeb4
felixslama <firejava@protonmail.com>
2023-03-13 22:44:04 +0100

    upload api working, test using rest-client vscode-addon not working yet.

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
-from s3 import upload_file
-# uvicorn main:app --reload
+from s3 import *
 app = FastAPI()
 
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-
-@app.post("/upload")
-async def uploadfile():
-    return {"message":upload_file("testfile.txt","airtrack-test")}
-
-@app.post("/files/")
-async def create_file(file: bytes | None = File(default=None)):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
-
-
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile | None = None):
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"filename": file.filename}
+async def create_upload_file(file: UploadFile = File(...)):
+    upload_file_to_s3(file)
